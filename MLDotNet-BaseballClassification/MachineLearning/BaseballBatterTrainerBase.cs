9cb88004800d82bd5f4e74a7972cfb4b63dcc660
Bart Czernicki <bartczernicki@outlook.com>
2021-08-16 21:45:10 +0000

    Refactoring training pipelines.

--- MLDotNet-BaseballClassification/MachineLearning/BaseballBatterTrainerBase.cs
+++ MLDotNet-BaseballClassification/MachineLearning/BaseballBatterTrainerBase.cs
@@ -16,6 +16,14 @@ namespace MLDotNet_BaseballClassification.MachineLearning
     public abstract class BaseballBatterTrainerBase<TParameters> : ITrainerBase
         where TParameters : class
     {
+        private int _seed = 100;
+
+        private static string[] _featureColumns = new string[] {
+            "YearsPlayed", "AB", "R", "H", "Doubles", "Triples", "HR", "RBI", "SB",
+            "BattingAverage", "SluggingPct", "AllStarAppearances", "TB", "TotalPlayerAwards"
+            // Other Features
+            /*, "MVPs", "TripleCrowns", "GoldGloves", "MajorLeaguePlayerOfTheYearAwards"*/
+        };
 
         public string Name { get; protected set; }
 
@@ -24,7 +32,7 @@ namespace MLDotNet_BaseballClassification.MachineLearning
 
         protected readonly MLContext _mlContext;
 
-        protected string[] _featureColumns;
+
 
         protected DataOperationsCatalog.TrainTestData _dataSplit;
         protected ITrainerEstimator<BinaryPredictionTransformer<TParameters>, TParameters> _model;
@@ -32,7 +40,7 @@ namespace MLDotNet_BaseballClassification.MachineLearning
 
         protected BaseballBatterTrainerBase()
         {
-            _mlContext = new MLContext(100);
+            _mlContext = new MLContext(_seed);
         }
 
         /// <summary>
@@ -74,8 +82,10 @@ namespace MLDotNet_BaseballClassification.MachineLearning
 
         private EstimatorChain<NormalizingTransformer> GetBaseLinePipeline()
         {
+            // Build baseline platform and cache
             var baselineTransform = _mlContext.Transforms.Concatenate("FeaturesBeforeNormalization", _featureColumns)
-                .Append(_mlContext.Transforms.NormalizeMinMax("Features", "FeaturesBeforeNormalization"));
+                .Append(_mlContext.Transforms.NormalizeMinMax("Features", "FeaturesBeforeNormalization"))
+                .AppendCacheCheckpoint(this._mlContext);
 
             return baselineTransform;
         }
@@ -85,6 +95,7 @@ namespace MLDotNet_BaseballClassification.MachineLearning
             var trainingDataView = _mlContext.Data
                                     .LoadFromTextFile<MLBBaseballBatter>
                                       (trainingFileName, hasHeader: true, separatorChar: ',');
+
             return _mlContext.Data.TrainTestSplit(trainingDataView, testFraction: 0.3);
         }
     }
