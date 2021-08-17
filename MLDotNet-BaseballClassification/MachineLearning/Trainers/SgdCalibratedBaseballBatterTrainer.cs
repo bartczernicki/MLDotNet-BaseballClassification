acd746d2a450d54a87adc5f52e181a7f1b4bf6ff
Bart Czernicki <bartczernicki@outlook.com>
2021-08-17 20:39:24 +0000

    Refactoring training pipelines.

---
+++ MLDotNet-BaseballClassification/MachineLearning/Trainers/SgdCalibratedBaseballBatterTrainer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Calibrators;
+using Microsoft.ML.Trainers;
+
+namespace MLDotNet_BaseballClassification.MachineLearning.Trainers
+{
+    public class SgdCalibratedBaseballBatterTrainer : BaseballBatterTrainerBase<
+                  CalibratedModelParametersBase<LinearBinaryModelParameters, PlattCalibrator>>
+    {
+        public SgdCalibratedBaseballBatterTrainer(string labelColumnName, int numberOfIterations = 10)
+    : base()
+        {
+            this.AlgorithmName = "SgdCalibrated";
+            this.Name = $"SgdCalibrated-{labelColumnName}|{numberOfIterations}";
+            this.LabelColumnName = labelColumnName;
+
+            _trainerEstimator = _mlContext.BinaryClassification.Trainers.SgdCalibrated(labelColumnName: labelColumnName, 
+                numberOfIterations: numberOfIterations);
+        }
+    }
+}
